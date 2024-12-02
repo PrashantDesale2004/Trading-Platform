36c2faa3cd1f62f04910d17be01a651a2f2a5f77
PrashantDesale2004 <prashantdesale2004@gmail.com>
2024-12-03 01:05:16 +0530

    complete singup

--- src/main/java/com/Prash/Controller/AuthController.java
+++ src/main/java/com/Prash/Controller/AuthController.java
@@ -1,10 +1,15 @@
 package com.Prash.Controller;
 
+import com.Prash.Config.JwtProvider;
 import com.Prash.Repository.UserRepository;
+import com.Prash.Response.AuthResponse;
 import com.Prash.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +21,35 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
     @PostMapping("/signup")
-    public ResponseEntity<User>  register(@RequestBody User user){
+    public ResponseEntity<AuthResponse>  register(@RequestBody User user) throws Exception {
+
+
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+        if(isEmailExist != null){
+            throw new Exception("user already used with another account");
+        }
         User newUser = new User();
         newUser.setEmail(user.getEmail());
         newUser.setPassword(user.getPassword());
         newUser.setEmail(user.getEmail());
         newUser.setFullName(user.getFullName());
 
-        User isEmailExist = userRepository.findByEmail(user.getEmail());
-        if(isEmailExist != null){
-            throw new Exception("user already used with another account");
-        }
         User saveUser = userRepository.save(newUser);
-        return new ResponseEntity<>(saveUser, HttpStatus.CREATED);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                user.getEmail(),
+                user.getPassword()
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        String jwt = JwtProvider.generateToken(auth);
+
+        AuthResponse res =new AuthResponse();
+        res.setJwt(jwt);
+        res.setStatus(true);
+        res.setMessage("register success");
+        return new ResponseEntity<>(res, HttpStatus.CREATED);
 
     }
 }
